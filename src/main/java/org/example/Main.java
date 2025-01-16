dfc0b9411d5e3375718575ce616146d27326e057
UncleWang <wangzanquan@yahoo.com>
2025-01-15 21:31:47 -0500

    Chapter 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
+        Parrot p = context.getBean(Parrot.class);
+
+        System.out.println(p);
+        System.out.println(p.getName());
+
+//        Parrot p = context.getBean("parrot2",Parrot.class);
+//        System.out.println(p.getName());
+//
+//        String h = context.getBean(String.class);
+//        System.out.println(h);
+//
+//        Integer n = context.getBean(Integer.class);
+//        System.out.println(n);
     }
 }
---
+++ src/main/java/org/example/config/ProjectConfig.java
@@ -0,0 +1,11 @@
+package org.example.config;
+
+import org.example.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.example")
+public class ProjectConfig {
+}
